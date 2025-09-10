45602ab4d0b177d7cba974bad70d9a2b46689cce
Themalind <therese.m.lindberg@gmail.com>
2025-09-10 13:23:02 +0200

    Skapat förutsättningar för auth

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import { db } from "./prisma/db";
+
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: "mongodb", // or "mysql", "postgresql", ...etc
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+});
